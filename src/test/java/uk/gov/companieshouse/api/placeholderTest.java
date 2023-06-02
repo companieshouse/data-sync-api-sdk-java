3412b1ddd6bd535494ec1167d23c98f84727b48f
maddytjCH <mtjalma@companieshouse.gov.uk>
2023-06-02 13:14:10 +0100

    add placeholder test

---
+++ src/test/java/uk/gov/companieshouse/api/placeholderTest.java
@@ -0,0 +1,12 @@
+package uk.gov.companieshouse.api;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class placeholderTest {
+    @Test
+    public void placeholder() {
+        assertTrue(true);
+    }
+}
