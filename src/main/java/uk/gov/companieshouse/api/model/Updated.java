41e1f3439f382b2404a1b0952a5cd19d3be415c0
lbedrosyanCH <lbedrosyan@companieshouse.gov.uk>
2023-07-07 14:59:43 +0100

    Modified Updated adding optional fields by and type. Moved CompanyProfileDocument from company profile. DSND-1845

--- src/main/java/uk/gov/companieshouse/api/model/Updated.java
+++ src/main/java/uk/gov/companieshouse/api/model/Updated.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 public class Updated {
 
     private LocalDateTime at;
+    private String by;
+    private String type;
 
     public LocalDateTime getAt() {
         return at;
@@ -14,4 +16,36 @@ public class Updated {
         this.at = at;
         return this;
     }
+
+    public String getBy(){
+        return by;
+    }
+
+    public Updated setBy(String by) {
+        this.by = by;
+        return this;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public Updated setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public Updated() {
+    }
+
+    public Updated(LocalDateTime at) {
+        this.at = at;
+    }
+
+    public Updated(LocalDateTime at, String by, String type) {
+        this.at = at;
+        this.by = by;
+        this.type = type;
+    }
+
 }
